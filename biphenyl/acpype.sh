78786bad515459424326f65c09fd2cf1b9f8a014
Michio Katouda <katouda@rist.or.jp>
2021-04-22 21:49:50 +0900

    Add thread environmetal values

--- biphenyl/acpype.sh
+++ biphenyl/acpype.sh
@@ -3,6 +3,11 @@
 smi="c1ccc(cc1)c2ccccc2"
 name=biphenyl
 
+export MKL_NUM_THREADS=1
+export OMP_NUM_THREADS=1,1
+export OMP_STACKSIZE=256MB
+ulimit -s unlimited
+
 obabel -:${smi} -omol -O ${name}.mol --gen3d
 xtb ${name}.mol --opt
 obabel -imol xtbopt.mol -omol2 -O ${name}.mol2
